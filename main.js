4691e4291b57ec501dbfc8e277903490cb96cd49
Lorena Kelczeski Macari <108080627+lkm12sk@users.noreply.github.com>
2024-05-17 22:27:26 -0300

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const botões = document.querySelectorAll(".botão");
 
+  for(let a=0;a<botões.length;a++){
+    botões[a].onclick = function{
+
+      for{let b=0;b<botões.length;b++){
+        botões[b].classList.remove("ativo");
+      }
+
+botões[a].classList.add("ativo");
+         }
+    }
